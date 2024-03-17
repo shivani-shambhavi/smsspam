b9685672e86851961d3fb17008268265b18380db
shivani-shambhavi <163621828+shivani-shambhavi@users.noreply.github.com>
2024-03-17 16:48:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,18 @@ import pickle
 app = Flask(__name__)
 
 # Load the trained model
-with open('Naive_model1.pkl', 'rb') as model_file:
+with open('Naive_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        # Get the email input from the form
-        email = request.form['email']
-        
-        # Perform classification using the loaded model
-        prediction = model.predict([email])[0]
-        
-        # Return the classification result
-        return render_template('index.html', email=email, prediction=prediction)
-    else:
-        return render_template('index.html', email='', prediction='')
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    email = request.form.get('email', '')
+    prediction = model.predict([email])[0]
+    return render_template('result.html', email=email, prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
